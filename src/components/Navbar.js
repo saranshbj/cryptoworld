19aef6e201a44bb95cf2d91e24fb5a5f34e223b9
Saransh Bhardwaj <49323905+saranshbj@users.noreply.github.com>
2023-07-04 22:37:29 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {FaCoins} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <Link to='/'>
+            <div className='navbar'>
+                <FaCoins className='icon' />
+                <h1 className='purple'>CRYPTOWORLD.</h1>
+            </div>
+        </Link>
+    )
+}
+
+export default Navbar
